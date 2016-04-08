2c89da5c9470f61de1d7e60c6f24555a7077dbe3
sszlm <zxt1412@163.com>
2016-04-08 19:44:31 +0800

    form1

--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using MissionPlanner.Controls;
+using GMap.NET.WindowsForms;
+using MissionPlanner.GCSViews;
 
 namespace MissionPlanner
 {
     public partial class Form1 : Form
     {
+
+        GMapOverlay drawnpolygonsoverlay = new GMapOverlay("drawnpolygons");
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
+
+            int i, k;
+            double o;
+            o = MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lat;
+            k = FlightPlanner.k;
+            dataGridView1.RowCount = k + 1;
+            string h = "50";
+            InputBox.Show("海拔", "海拔", ref h);
+            for (i = 0; i < k; i++)
+            {
+                MainV2.instance.FlightPlanner.polygongridmode = false;
+                MainV2.instance.FlightPlanner.AddWPToMap(MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lat, MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lng, Int32.Parse(h));
+                MainV2.instance.FlightPlanner.setfromMap(MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lat, MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lng, Int32.Parse(h));
+            }
+            MainV2.instance.FlightPlanner.AddWPToMap(MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lat, MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lng, Int32.Parse(h));
+            MainV2.instance.FlightPlanner.setfromMap(MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lat, MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lng, Int32.Parse(h));
+            for (i = 0; i < k; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = i + 1;
+                dataGridView1.Rows[i].Cells[1].Value = MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lng;
+                dataGridView1.Rows[i].Cells[2].Value = MainV2.instance.FlightPlanner.drawnpolygon.Points[i].Lat;
+            }
+            dataGridView1.Rows[k].Cells[0].Value = k + 1;
+            dataGridView1.Rows[k].Cells[1].Value = MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lng;
+            dataGridView1.Rows[k].Cells[2].Value = MainV2.instance.FlightPlanner.drawnpolygon.Points[0].Lat;
+            Show();
+            MainV2.instance.FlightPlanner.drawnpolygon.Points.Clear();
+            MainV2.instance.FlightPlanner.MainMap.Invalidate();
+
+            MainV2.instance.FlightPlanner.writeKML();
 
         }
 
@@ -25,5 +56,10 @@ namespace MissionPlanner
         {
             this.Close();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
     }
 }
+
